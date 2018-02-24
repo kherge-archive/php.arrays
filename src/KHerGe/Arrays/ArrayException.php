4ed89611a49f0b568b4a4b84d35bc790f4ee0a7d
Kevin Herrera <kevin@herrera.io>
2018-02-24 15:31:30 -0800

    Creating an exception for array implementations.

---
+++ src/KHerGe/Arrays/ArrayException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KHerGe\Arrays;
+
+use KHerGe\Exception\AbstractException;
+
+/**
+ * An exception that is thrown for an array related error.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class ArrayException extends AbstractException
+{
+}
